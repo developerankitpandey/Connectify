a93cde1d7292e7e2823992aaba5d002aea0f75b0
Ankit Pandey <iamankitpandey801@gmail.com>
2024-06-07 14:29:50 +0530

    added home index page

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
+  root "home#index"
+  resources :home, only: [:index] # Limiting routes to only index if that's the intent
+end
